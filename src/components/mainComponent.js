611d9fe415295ae951687e5bb21be64c082f4dc7
infinity2567 <jlopes5000@gmail.com>
2025-05-09 17:30:06 +0100

    implemented chat functionality

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,32 +1,42 @@
 import { useEffect, useState } from "react";
 import { useOllamaStreaming } from "../utils";
+import "./mainComponent.css";
 
 export default function MainComponent() {
   const [messages, setMessages] = useState([]);
   const { output, loading, send } = useOllamaStreaming();
 
+  useEffect(() => {
+    if (!loading && output) {
+      setMessages((prev) => [...prev, { role: "assistant", content: output }]);
+    }
+  }, [loading, output]);
+
   function submitMessage(e) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const chatTurn = formData.get("msg");
+    const userMsg = {
+      role: "user",
+      content: formData.get("msg") + " (reply using only one sentence)",
+    };
 
-    setMessages((prev) => [...prev, chatTurn]);
     e.currentTarget.reset();
 
-    send("llama3.2", [{ role: "user", content: chatTurn }]);
+    send("gemma3:4b-it-qat", [...messages, userMsg]);
+    setMessages((prev) => [...prev, userMsg]);
   }
 
   return (
     <>
-      <h1>Test App</h1>
+      <h1>LLM Chat</h1>
       <form onSubmit={submitMessage}>
         <label>Input</label>
-        <textarea name="msg"></textarea>
+        <input name="msg"></input>
         <button type="submit">Send</button>
       </form>
       <ul>
         {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
+          <li key={index}>{msg.role + ": " + msg.content}</li>
         ))}
       </ul>
       <div>
